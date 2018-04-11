2576fd22fcd5d837fd0b0fb559d54c4cfd2226cb
Abro1986 <AndrewBroestl@gmail.com>
2018-04-10 18:16:13 -0600

    again

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ let parachute = document.getElementById('parachute')
 let wolly = document.getElementById('wolly')
 let ehc = document.getElementById('ehc')
 
-var getUrl = window.location;
-var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
-
-console.log(baseUrl)
-
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+lastFish = -1;
 let cutty = "yellowstone.png"
 let brown = "brown.png"
 let rainbow = "rainbow.png"
@@ -16,18 +14,6 @@ let brook = "brook.png"
 let wollySrc = "wolly.png"
 let paraSrc = "parachute.png"
 let ehcSrc = "EHC.png"
-
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-lastFish = -1;
-// let cutty = "yellowstone.png"
-// let brown = "brown.png"
-// let rainbow = "rainbow.png"
-// let brook = "brook.png"
-// let wollySrc = "wolly.png"
-// let paraSrc = "parachute.png"
-// let ehcSrc = "EHC.png"
 const fly = [];
 const maxFly = 15;
 //let collision = distance(fish[i].x, fish[i].y, fly[0].x, fly[0].y) 
@@ -37,7 +23,6 @@ const maxFly = 15;
 //     x: innerWidth ,
 //     y: innerHeight
 // }
-
 
 for(i = 0; i < maxFly; i++) {
     fly.push({
@@ -49,25 +34,24 @@ for(i = 0; i < maxFly; i++) {
 
 let pic2 = new Image();
 let pic = new Image();
-//pic.src = "rainbow.png"
-//pic2.src= parachute.src
+
  
   
 pic.addEventListener("load", addFish)
 
 parachute.addEventListener("click", function(e) {
 
-  pic2.src = parachuteSrc
+  pic2.src = this.src
 })
 
 wolly.addEventListener('click', function(e) {
 
-  pic2.src = wollySrc
+  pic2.src = wolly.src
 })
 
 ehc.addEventListener('click', function(e) {
   
-  pic2.src = ehcSrc
+  pic2.src = ehc.src
 })
 
 canvas.addEventListener("click", function(e) {
@@ -93,7 +77,7 @@ function randomFish() {
       pic: new Image(),
        
       x: 100,
-      y: (Math.random() + 500) + (Math.random() * 250),
+      y: Math.random() * (window.innerHeight - 100),
       width: 130,
       height: 75,
   }
@@ -101,13 +85,13 @@ function randomFish() {
     //let randomNum = Math.random()
 
   if (randomNum2 < 0.50) {     
-    trout.pic.src = rainbow;
+    trout.pic.src = "rainbow.png";
     } else if (randomNum2 > 0.50 && randomNum2 < 1) {      
-    trout.pic.src = brook;
+    trout.pic.src = "brook.png";
     } else if (randomNum2 > 1 && randomNum2 < 1.50) {
-    trout.pic.src = brown;
+    trout.pic.src = "brown.png";
     } else {
-    trout.pic.src = cutty;
+    trout.pic.src = "yellowstone.png"
     }
     
    fish.push(trout);
@@ -123,17 +107,13 @@ function distance(x1, y1, x2, y2) {
 }
 
 function brownEat() {
-
-  //canvas2d.drawImage(pic2, fly[0].x, fly[0].y, 40, 40);
     
   for (i = 0; i < fish.length; i++) {
 
-    if ((distance(fish[i].x, fish[i].y, fly[0].x, fly[0].y) < 60) && (fish[i].pic.src.split("/").pop() === brown) && (pic2.src.split("/").pop() === wollySrc)) {
+    if ((distance(fish[i].x, fish[i].y, fly[0].x, fly[0].y) < 60) && (fish[i].pic.src === brown) && (pic2.src === wollySrc)) {
     fish.splice(i, 1);
     fly.splice(0, 1);
-
-    console.log(fish[i])
-    //pic2 = new Image()
+    pic2 = new Image()
     }
   }
 }
@@ -145,7 +125,7 @@ function rainbowEat() {
     if ((distance(fish[i].x, fish[i].y, fly[0].x, fly[0].y) < 60) && (fish[i].pic.src === rainbow) && (pic2.src === paraSrc)) {
     fish.splice(i, 1);
     fly.splice(0, 1);
-    //pic2 = new Image()
+    pic2 = new Image()
     }
   }
 }
@@ -157,7 +137,7 @@ function brookEat() {
     if ((distance(fish[i].x, fish[i].y, fly[0].x, fly[0].y) < 60) && (fish[i].pic.src === brook) && (pic2.src === ehcSrc)) {
     fish.splice(i, 1);
     fly.splice(0, 1);
-    //pic2 = new Image()
+    pic2 = new Image()
     }
   }
 }
@@ -166,50 +146,37 @@ function cuttyEat() {
 
   for (i = 0; i < fish.length; i++) {
 
-    if ((distance(fish[i].x, fish[i].y, fly[0].x, fly[0].y) < 60) && (fish[i].pic === cutty) && (pic2.src === paraSrc)) {
+    if ((distance(fish[i].x, fish[i].y, fly[0].x, fly[0].y) < 60) && (fish[i].pic.src === cutty) && (pic2.src === paraSrc)) {
     fish.splice(i, 1);
     fly.splice(0, 1);
-    //pic2 = new Image()
+    pic2 = new Image()
     }
   }
 }
 
 function addFish() {
 
-  
+    let time = Date.now();
 
-  let time = Date.now();
-
-  if(time > (lastFish + 2000)) {
-     lastFish = time;
-     randomFish();
+    if(time > (lastFish + 2000)) {
+      lastFish = time;
+      randomFish();
   }
 
   canvas2d.clearRect(0, 0, innerWidth, innerHeight);
-     //canvas2d.beginPath();
-
-
+     
   requestAnimationFrame(addFish);
 
 
+  for (let i = 0; i < fish.length; i++) {
+      let trout = fish[i];
+      canvas2d.drawImage(trout.pic, trout.x, trout.y, trout.width, trout.height);
+         //canvas2d.drawImage(f.pic, f.x, f.y, f.width, f.height);         
+  }
+    
+    canvas2d.drawImage(pic2, fly[0].x, fly[0].y, 40, 40);
+    
 
-    for (let i = 0; i < fish.length; i++) {
-          let trout = fish[i];
-          canvas2d.drawImage(trout.pic, trout.x, trout.y, trout.width, trout.height);
-         //canvas2d.drawImage(f.pic, f.x, f.y, f.width, f.height);
-         
-    }
-    canvas2d.drawImage(pic2, fly[0].x, fly[0].y, 80, 80);
-    // for (i = 0; i < fish.length; i++) {
-
-    //  if ((distance(fish[i].x, fish[i].y, fly[0].x, fly[0].y) < 60) && (fish[i].pic.src === cutty) && (pic2.src === paraSrc || wollySrc)) {
-    //   fish.splice(i, 1);
-    //   fly.splice(0, 1);
-    //   pic2 = new Image()
-
-      
-    //  }
-    //}
     brownEat()
     rainbowEat()
     brookEat()
@@ -217,24 +184,9 @@ function addFish() {
       
 }
 
-function moveFish() {
-
-  for(let i = 0; i < fish.length; i++) {
-    let f = fish[i];
-
-    f.x += (Math.random() * 5);
-
-     if (f.x > canvas.width) {
-       // fish[i] = {
-       // pic: pic,
-       // x: Math.random() * 100,
-       // y: Math.random() * (window.innerHeight - 100),
-       // width: 30,
-       // height: 30,
-       // }
-       fish.splice(0, 1);
-
-      
+function moveFish() {"if (f.x > canvas.width) {
+       
+      fish.splice(0, 1);       
      }
   }
 }
