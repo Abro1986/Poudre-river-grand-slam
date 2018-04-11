3d3daf7a8b0b503ba0b4e506ab81488282600b51
Abro1986 <AndrewBroestl@gmail.com>
2018-04-11 09:10:45 -0600

    last commit

--- splash.js
+++ splash.js
@@ -6,5 +6,5 @@ console.log("working")
 
 $("#info").click(function() {
 	console.log("hello")
-	$("#container").append("<li>hello</li>")
+	$("#container").append("<h2>The object of the game is to catch as many fish as you can in 60 seconds. Pick a fly from the top left hand side and place it with a mouse click in front of the fish. differet species will eat different things so be mindful of what ate what.</h2>")
 })
